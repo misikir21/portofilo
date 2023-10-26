b881a142006f40b10b8e3a0519038220ba69a508
misikir21 <tekamisikir.com>
2023-10-26 06:25:33 -0700

    complete projcet strcture

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,28 @@ import {
   Navbar,
   Tech,
   Works,
-  StarsCanvs,
+  StarsCanvas,
 } from "./components";
 const App = () => {
   return (
     <BrowserRouter>
-<div className="relative z-0 bg-primary ">
-  <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center"></div>
-</div>
-    <BrowserRouter/>
-  )
+      <div className="relative z-0 bg-primary ">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <StarsCanvas />
+          <Contact />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 };
 
 export default App;
