80be6d238ba483a9631bd7a42734b4dfda07e0c4
misikir21 <tekamisikir.com>
2023-10-26 06:19:30 -0700

    add compo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { BrowserRouter } from "react-router-dom";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvs,
+} from "./components";
 const App = () => {
-  return <div>3d developer portofilo</div>;
+  return (
+    <BrowserRouter>
+<div className="relative z-0 bg-primary ">
+  <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center"></div>
+</div>
+    <BrowserRouter/>
+  )
 };
 
 export default App;
