daa936bf6e67141fc25d78fdbe91fc8db1d0dd45
misikir21 <tekamisikir.com>
2023-10-27 23:37:29 -0700

    loop through servies list

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,6 +4,9 @@ import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
+const ServiceCard = ({ title, index, icon }) => {
+  return <p>{title}</p>;
+};
 const About = () => {
   return (
     <>
@@ -11,13 +14,21 @@ const About = () => {
         <p className={styles.sectionSubText}>Introduction</p>
         <h2 className={styles.sectionHeadText}>Overview</h2>
       </motion.div>
-      <motion.p variants={fadeIn("", "", 0.1, 1)}>
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-d text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
         I'm a skilled software developer with experience in TypeScript and
         JavaScript, and expertise in frameworks like React, Node.js, and
         Three.js. I'm a quick learner and collaborate closely with clients to
         create efficient, scalable, and user-friendly solutions that solve
         real-world problems. Let's work together to bring your ideas to life!
       </motion.p>
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={services.title} index={index} {...service} />
+        ))}
+      </div>
     </>
   );
 };
