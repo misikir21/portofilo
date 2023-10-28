b70de06b3a6d787f3cfb4f01008c6b76e09b4fc7
misikir21 <tekamisikir.com>
2023-10-28 00:06:33 -0700

    update the about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,27 @@
 import React from "react";
-import { Tilt } from "react-tilt";
+import Tilt from "react-tilt";
 import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 const ServiceCard = ({ title, index, icon }) => {
-  return <p>{title}</p>;
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450,
+          }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col "
+        />
+      </motion.div>
+    </Tilt>
+  );
 };
 const About = () => {
   return (
