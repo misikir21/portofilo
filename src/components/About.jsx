b54ca9ca6ed068ce5c5d527d6e0b0f68801e7928
misikir21 <tekamisikir.com>
2023-10-27 23:17:22 -0700

    add p motion and header motion

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,10 +2,24 @@ import React from "react";
 import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
 import { styles } from "../styles";
-import { services } from "../constant";
+import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview</h2>
+      </motion.div>
+      <motion.p variants={fadeIn("", "", 0.1, 1)}>
+        I'm a skilled software developer with experience in TypeScript and
+        JavaScript, and expertise in frameworks like React, Node.js, and
+        Three.js. I'm a quick learner and collaborate closely with clients to
+        create efficient, scalable, and user-friendly solutions that solve
+        real-world problems. Let's work together to bring your ideas to life!
+      </motion.p>
+    </>
+  );
 };
 
 export default About;
