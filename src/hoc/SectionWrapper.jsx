03b943e365cc5e9182327f06739039be36840b3c
misikir21 <tekamisikir.com>
2023-11-02 10:55:11 +0300

    complete the about section

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -1,20 +1,25 @@
 import { motion } from "framer-motion";
-import { styles } from "../../styles";
-import { staggerContainer } from "../../utils/motion";
-const SectionWrapper = (Component, idName) => {
+
+import { styles } from "../styles";
+import { staggerContainer } from "../utils/motion";
+
+const StarWrapper = (Component, idName) =>
   function HOC() {
     return (
       <motion.section
         variants={staggerContainer()}
-        inital="hidden"
-        whiileInView="show"
+        initial="hidden"
+        whileInView="show"
         viewport={{ once: true, amount: 0.25 }}
         className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
       >
+        <span className="hash-span" id={idName}>
+          &nbsp;
+        </span>
+
         <Component />
       </motion.section>
     );
-  }
-};
+  };
 
-export default SectionWrapper;
+export default StarWrapper;
