35578ab5680005afa2ed8bfaeee14335cedf1e29
misikir21 <tekamisikir.com>
2023-11-02 11:01:33 +0300

    import the required moudules to comp

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-
+import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import { motion } from "framer-motion";
+import "react-vertical-timeline-component/style.min.css";
 const Experience = () => {
-  return (
-    <div>Experience</div>
-  )
-}
+  return <div>Experience</div>;
+};
 
-export default Experience
+export default Experience;
