794581cf46dd7c13a513b59aebd345b5347e11c4
misikir21 <tekamisikir.com>
2023-11-02 11:11:30 +0300

    add work experience content with wrapper

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -5,8 +5,17 @@ import {
 } from "react-vertical-timeline-component";
 import { motion } from "framer-motion";
 import "react-vertical-timeline-component/style.min.css";
+import { styles } from "../styles";
+import { experiences } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { textVariant } from "../utils/motion";
 const Experience = () => {
-  return <div>Experience</div>;
+  return (
+    <motion.div variants={textVariant()}>
+      <p className={styles.sectionSubText}>What i have done so far</p>
+      <h2 className={styles.sectionHeadText}>Work Experience</h2>
+    </motion.div>
+  );
 };
 
-export default Experience;
+export default SectionWrapper(Experience, "work");
