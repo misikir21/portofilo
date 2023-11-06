ed20754205c4779ed619456f429d3a3233b41813
misikir21 <tekamisikir.com>
2023-11-06 14:30:13 +0300

    add more styles to p and h3 tag

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -44,8 +44,8 @@ const ProjectCard = ({
           </div>
         </div>
         <div className="mt-5">
-          <h3>{name}</h3>
-          <p>{description}</p>
+          <h3 className="text-white font-bold text-[24px]">{name}</h3>
+          <p className="mt-2 text-secondary text-[14px]">{description}</p>
         </div>
       </Tilt>
     </motion.div>
