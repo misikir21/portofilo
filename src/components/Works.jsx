e0c567877250b5f57cb727606305434c19b8418c
misikir21 <tekamisikir.com>
2023-11-06 10:21:57 +0300

    render the works component and wrap it

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { github } from "../assets";
-import { sectionWrapper } from "../hoc";
+import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 
@@ -10,15 +10,10 @@ const Works = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-        <p className={`${styles.sectionSubText} text-center`}>
-          What I have done so far
-        </p>
-        <h2 className={`${styles.sectionHeadText} text-center`}>
-          Work Experience.
-        </h2>
+        <p className={`${styles.sectionSubText} text-center`}>My Work</p>
+        <h2 className={`${styles.sectionHeadText} text-center`}>Projects</h2>
       </motion.div>
     </>
   );
 };
-
-export default sectionWrapper("work", "");
+export default SectionWrapper(Works, "");
