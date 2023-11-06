fcf9293e64b9b2e9ae7cf1f5b08719720c251ccd
misikir21 <tekamisikir.com>
2023-11-06 10:30:51 +0300

    add motion p div

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -13,6 +13,17 @@ const Works = () => {
         <p className={`${styles.sectionSubText} text-center`}>My Work</p>
         <h2 className={`${styles.sectionHeadText} text-center`}>Projects</h2>
       </motion.div>
+      <div className="w-full flex">
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        >
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis
+          debitis nisi sed, autem laborum ipsam, enim excepturi , neque possimus
+          provident deleniti laudantium quo ratione iure. Ad quisquam possimus
+          unde in, sit illo eaque dolores quos!
+        </motion.p>
+      </div>
     </>
   );
 };
