b1a2916824fb044d7ea5d1d4318296abd0cdf1b0
misikir21 <tekamisikir.com>
2023-11-06 11:18:21 +0300

    add a tilt components

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -5,6 +5,7 @@ import { github } from "../assets";
 import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
+import { Tilt } from "react-tilt";
 const ProjectCard = ({
   index,
   name,
@@ -13,7 +14,18 @@ const ProjectCard = ({
   image,
   source_code_link,
 }) => {
-  return <motion.div variants={fadeIn("up","spring" index * 0.5,0.75)}>test</motion.div>;
+  return <motion.div variants={fadeIn("up","spring" index * 0.5,0.75)}>
+    <Tilt 
+    options={{
+      max:45,
+      scale:1,
+      speed:450
+    }}
+    className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+    >
+
+    </Tilt>
+  </motion.div>;
 };
 const Works = () => {
   return (
