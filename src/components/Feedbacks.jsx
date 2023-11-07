f0f71d90fd29249280bb6cbaef2f2dc504831673
misikir21 <tekamisikir.com>
2023-11-07 11:12:18 +0300

    add p and h2 elements

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { styles } from "../styles";
-import { SectionWrapper } from "./hoc";
-import { fadeIn, textVariant } from "../utils";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
 import { testimonials } from "../constants";
 const Feedbacks = () => {
   return (
     <div className="mt-12 bg-black rounded-[20px]">
       <div
         className={`${styles.padding} bg-tertiary rounded-2xl min-h-[300px]`}
-      ></div>
+      >
+        <motion.div variants={textVariant()}>
+          <p className={styles.sectionSubText}>What others say</p>
+          <h2 className={styles.sectionHeadText}>Testimonails</h2>
+        </motion.div>
+      </div>
     </div>
   );
 };
