2464ece748ca8d9548adc118a65f433d8bf5805e
misikir21 <tekamisikir.com>
2023-11-07 10:58:14 +0300

    add empty card

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -2,10 +2,16 @@ import React from "react";
 import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { SectionWrapper } from "./hoc";
-import { fadeib, textVariant } from "../utils";
+import { fadeIn, textVariant } from "../utils";
 import { testimonials } from "../constants";
 const Feedbacks = () => {
-  return <div>Feedbacks</div>;
+  return (
+    <div className="mt-12 bg-black rounded-[20px]">
+      <div
+        className={`${styles.padding} bg-tertiary rounded-2xl min-h-[300px]`}
+      ></div>
+    </div>
+  );
 };
 
 export default Feedbacks;
