f025fe8293d6cedc72a7721f9110fe215fa9bc98
misikir21 <tekamisikir.com>
2023-11-07 15:10:47 +0300

    add the images to each card

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -29,6 +29,11 @@ const FeedbackCard = ({
             {designation} of {company}
           </p>
         </div>
+        <img
+          src={image}
+          alt={`feedback-by-${name} `}
+          className="w-10 h-10 rounded-full object-cover"
+        />
       </div>
     </div>
   </motion.div>
