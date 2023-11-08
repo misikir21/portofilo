cb2abb9d622fbf7bca98c1e58480c6971a9fa59b
misikir21 <misikirkaka@gmail.com>
2023-11-08 16:40:11 +0300

    add p and h3 element

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -20,7 +20,10 @@ const Contact = () => {
       <motion.div
         variants={slideIn("left", "tween", 0.2, 1)}
         className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
-      ></motion.div>
+      >
+        <p className={styles.sectionSubText}>Get in touch</p>
+        <h3 className={styles.sectionHeadText}>Contact</h3>
+      </motion.div>
     </div>
   );
 };
