c47309bab7b09b1ecb769d64313a9f60d25d0182
misikir21 <tekamisikir.com>
2023-11-08 15:45:57 +0300

    fix all rendering issues

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,19 +3,21 @@ import { useState, useRef } from "react";
 import { motion } from "framer-motion";
 import emailjs from "@emailjs/browser";
 import { styles } from "../styles";
-import { earthcanvas } from "./canvas";
+import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 const Contact = () => {
-  const [form.setform]=useState({
-    name:'',
-    email:'',
-    message:'',
-  })
-const [load, setload] = useState(false)
-const handleChange=(e)=>{}
-const handleSubmit=(e)=>{}
-  return <div>Contact</div>;
+  const [form, setform] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [load, setload] = useState(false);
+  const handleChange = (e) => {};
+  const handleSubmit = (e) => {};
+  return (
+    <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden"></div>
+  );
 };
 
-export default Contact;
+export default SectionWrapper(Contact, "contact");
