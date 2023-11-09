6f53ff3ba7ac0f5da096776dbb3a7b15b5e245e6
misikir21 <misikirkaka@gmail.com>
2023-11-09 15:22:34 +0300

    fix some erros with emailjs

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -18,8 +18,8 @@ const Contact = () => {
   const [loading, setLoading] = useState(false);
 
   const handleChange = (e) => {
-    const { target } = e;
-    const { name, value } = target;
+    const { value, name } = e.target;
+    setForm({ ...form, [name]: value });
 
     setForm({
       ...form,
@@ -30,25 +30,25 @@ const Contact = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     setLoading(true);
-
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        "service_7z1jw7s",
+        "template_v4819rq",
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "misikir",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "misikir@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        " N2BzniXLT6c2VKZP3"
       )
       .then(
         () => {
           setLoading(false);
-          alert("Thank you. I will get back to you as soon as possible.");
-
+          alert(
+            "thank you for reaching me out i will contact you as soon as posible"
+          );
           setForm({
             name: "",
             email: "",
@@ -57,12 +57,42 @@ const Contact = () => {
         },
         (error) => {
           setLoading(false);
-          console.error(error);
-
-          alert("Ahh, something went wrong. Please try again.");
+          console.log(error);
+          alert("something went wrong");
         }
       );
   };
+  emailjs
+    .send(
+      import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+      import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+      {
+        from_name: form.name,
+        to_name: "JavaScript Mastery",
+        from_email: form.email,
+        to_email: "sujata@jsmastery.pro",
+        message: form.message,
+      },
+      import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+    )
+    .then(
+      () => {
+        setLoading(false);
+        alert("Thank you. I will get back to you as soon as possible.");
+
+        setForm({
+          name: "",
+          email: "",
+          message: "",
+        });
+      },
+      (error) => {
+        setLoading(false);
+        console.error(error);
+
+        alert("Ahh, something went wrong. Please try again.");
+      }
+    );
 
   return (
     <div
@@ -132,5 +162,4 @@ const Contact = () => {
     </div>
   );
 };
-
 export default SectionWrapper(Contact, "contact");
